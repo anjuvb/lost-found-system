11d07fdfbfb32b29e65291f5f58777e5776fa679
Akash0215 <akashkiran105@gmail.com>
2026-02-23 00:52:51 +0530

    Add AdminAction model for database

---
+++ admin_action.py
@@ -0,0 +1,10 @@
+from . import db
+
+class AdminAction(db.Model):
+    __tablename__ = "ADMIN_ACTION"
+
+    action_id = db.Column(db.Integer, primary_key=True)
+    action_taken = db.Column(db.String(100))
+    action_date = db.Column(db.Date)
+    admin_id = db.Column(db.Integer, db.ForeignKey("USER.user_id"))
+    claim_id = db.Column(db.Integer, db.ForeignKey("CLAIM.claim_id"))
